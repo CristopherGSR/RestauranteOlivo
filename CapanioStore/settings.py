f86f66c4cb54118b05f3b6c208e5c55e8861c129
CristopherGSR <crisgs608@gmail.com>
2022-12-04 12:42:27 -0500

    Sum

--- CapanioStore/settings.py
+++ CapanioStore/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'CapanioStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,9 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
@@ -131,6 +128,7 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 #Media Imagenes
 MEDIA_URL = '/images/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
