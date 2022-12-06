4c528b455b477602a1d0cd4020b051123a11c153
GonzaloAxelH <gonzaloaxelh@gmail.com>
2022-12-05 22:28:49 -0500

    add databnase postgrtess

--- CapanioStore/settings.py
+++ CapanioStore/settings.py
@@ -9,6 +9,7 @@ ENVIRONMENT = env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#DATABASE_URL=postgres://postgres:1234@127.0.0.1:5432/olivos
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
